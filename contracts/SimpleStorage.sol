245ade4fb8713c7f990c8df046daadf6f7421c16
unknown <zynk123@hotmail.com>
2021-11-29 12:10:21 +0100

    test

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -5,11 +5,11 @@ pragma solidity >=0.7.0 <0.9.0;
 contract SimpleStorage {
   uint value;
 
-  event storedDataChange(uint value); 
+  event isSet(uint value); 
 
   function set(uint x) public {
     value = x;
-    emit storedDataChange(value);
+    emit isSet(value);
   }
 
   function get() public view returns (uint) {
