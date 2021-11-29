ddcc617089d4d09566997e6b396419a72aab38c6
unknown <zynk123@hotmail.com>
2021-11-29 11:49:04 +0100

    Updates

---
+++ precache-manifest.b633bbb44385b71def0abf17b119315f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8ac8c21c1058b0cfe6e0ea596f8c33",
+    "url": "/truffle-react/index.html"
+  },
+  {
+    "revision": "b627d4306c75865b667f",
+    "url": "/truffle-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "030042701fc82e6d7ca4",
+    "url": "/truffle-react/static/js/2.ecd5f8d8.chunk.js"
+  },
+  {
+    "revision": "b627d4306c75865b667f",
+    "url": "/truffle-react/static/js/main.316ade0a.chunk.js"
+  },
+  {
+    "revision": "b350532b73f0ceede13e",
+    "url": "/truffle-react/static/js/runtime-main.7a7977a8.js"
+  }
+]);
